7ffc9953036517e77d7c08d6ed39e93b2469eb41
Lijiaqi1a <53029731+Lijiaqi1a@users.noreply.github.com>
2022-12-05 18:39:13 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+int main() {
+ ifstream file ("data.txt");
+  int calories = 0;
+  int total = 0;
+  string line;
+  while (getline(file, line)) {
+      if (line.empty()) {
+        if (calories > total) {
+          total = calories;
+        }
+        calories = 0;
+      }
+    else {
+      calories += stoi(line);
+    }
+  }
+  cout  << total;
+}
